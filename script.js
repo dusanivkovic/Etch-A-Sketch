b71c6b386514652d765ff2e457e80fb94181bf40
https://github.com/dusanivkovic <ivdule77@gmail.com>
2023-01-26 12:34:03 +0100

    Add Range tag

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 const fragment = document.createDocumentFragment();
 const container = document.querySelector('#container');
-for (let i = 0; i < 16; i++) {
-    const div = document.createElement('div');
-    div.classList.add('square')
-    fragment.append(div);
+let n = 4; 
+for (let i = 0; i < n; i++) {
+    const divRow = document.createElement('div');
+    divRow.classList.add('row');
+    fragment.append(divRow);
+    for (let j = 0; j < n; j++) {
+        const div = document.createElement('div');
+        div.classList.add('square')
+        divRow.append(div);
+        // fragment.append(div);
+    }
 }
 container.append(fragment);
 
+
 let square = document.getElementsByClassName('square');
 [...square].forEach(item => {
-    item.addEventListener('mouseover', e => {e.target.style.backgroundColor = 'gray'})
+    item.addEventListener('mouseover', e => {e.target.style.backgroundColor = 'gray'});
+    // item.addEventListener('mouseout', e => {e.target.style.backgroundColor = 'white'});
 })
 
 
